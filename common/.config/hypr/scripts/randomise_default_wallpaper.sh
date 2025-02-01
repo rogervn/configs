eb7a2c9df9c6a4ef78a9208c46b760e5dcce6fbb
Rogerio Vinhal Nunes <rogerio@vnunes.win>
2025-02-01 20:13:25 +0000

    fix randomise script

--- common/.config/hypr/scripts/randomise_default_wallpaper.sh
+++ common/.config/hypr/scripts/randomise_default_wallpaper.sh
@@ -4,7 +4,7 @@ WALLPAPER_DIR=~/.config/hypr/wallpaper
 DEFAULT_WALLPAPER_FILE=default.png
 
 cd $WALLPAPER_DIR
-random_wallpaper=$(find . ! -name $DEFAULT_WALLPAPER_FILE | shuf -n 1)
+random_wallpaper=$(find . ! -name $DEFAULT_WALLPAPER_FILE ! -name . | shuf -n 1)
 ln -sf $random_wallpaper $DEFAULT_WALLPAPER_FILE
 
 
